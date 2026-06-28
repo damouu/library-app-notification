d128a04c5ee565b964c9cca6b1fa9ce4c1f68a72
damouu <mouadsehbaoui@gmail.com>
2026-06-28 19:00:59 +0900

    Refactor: ChapterProjection.php (#LAN-5)

--- app/Models/ChapterProjection.php
+++ app/Models/ChapterProjection.php
@@ -15,28 +15,20 @@ class ChapterProjection extends Model
 
     protected $fillable = [
         'chapter_uuid',
-        'series_uuid',
         'title',
         'second_title',
-        'total_pages',
         'chapter_number',
-        'summary',
         'cover_artwork_url',
-        'publication_date',
     ];
 
     protected function casts(): array
     {
         return [
             'chapter_uuid' => 'string',
-            'series_uuid' => 'string',
             'title' => 'string',
             'second_title' => 'string',
-            'total_pages' => 'integer',
             'chapter_number' => 'integer',
-            'summary' => 'string',
             'cover_artwork_url' => 'string',
-            'publication_date' => 'date',
         ];
     }
 }
