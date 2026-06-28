1454c04ac02050c7a4f93c4553e204a62bf01c4d
damouu <mouadsehbaoui@gmail.com>
2026-06-28 18:57:59 +0900

    Refactor: 0001_01_03_000000_create_catalog_table.php (#LAN-5)

--- database/migrations/0001_01_03_000000_create_catalog_table.php
+++ database/migrations/0001_01_03_000000_create_catalog_table.php
@@ -14,14 +14,10 @@ return new class extends Migration {
         Schema::create('chapter_projection', function (Blueprint $table) {
             $table->id();
             $table->uuid('chapter_uuid')->unique()->index();
-            $table->uuid('series_uuid')->index();
             $table->string('title');
             $table->string('second_title')->nullable();
-            $table->unsignedInteger('total_pages');
             $table->unsignedInteger('chapter_number');
-            $table->text('summary')->nullable();
             $table->string('cover_artwork_url');
-            $table->date('publication_date');
             $table->timestamps();
             $table->softDeletes();
         });
