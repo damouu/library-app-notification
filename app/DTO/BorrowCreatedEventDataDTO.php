da314f0828ae711bd475e896e31d55d4b7f24af7
damouu <mouadsehbaoui@gmail.com>
2026-06-28 21:18:38 +0900

    Refactor: BorrowCreatedEventDataDTO.php (#LAN-6)

--- app/DTO/BorrowCreatedEventDataDTO.php
+++ app/DTO/BorrowCreatedEventDataDTO.php
@@ -8,11 +8,8 @@ final readonly class BorrowCreatedEventDataDTO
      * @param BorrowedItemDTO[] $borrowedItems
      */
     public function __construct(
-        public string $borrow_uuid,
-        public string $borrow_start_date,
-        public string $borrow_end_date,
-        public string $memberCardUuid,
         public string $borrowUuid,
+        public string $memberCardUuid,
         public string $borrowStartDate,
         public string $borrowEndDate,
         public array  $borrowedItems,
@@ -23,11 +20,8 @@ final readonly class BorrowCreatedEventDataDTO
     public static function fromArray(array $payload): self
     {
         return new self(
-            borrow_uuid: $payload['borrow_uuid'],
-            borrow_start_date: $payload['borrow_start_date'],
-            borrow_end_date: $payload['borrow_end_date'],
-            memberCardUuid: $payload['member_card_uuid'],
             borrowUuid: $payload['borrow_uuid'],
+            memberCardUuid: $payload['member_card_uuid'],
             borrowStartDate: $payload['borrow_start_date'],
             borrowEndDate: $payload['borrow_end_date'],
             borrowedItems: array_map(
