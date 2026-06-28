350c12b585b96c008b46c8fc62de195fe4e65846
damouu <mouadsehbaoui@gmail.com>
2026-06-28 18:59:00 +0900

    Refactor: books-list.blade.php (#LAN-5)

--- resources/views/components/books-list.blade.php
+++ resources/views/components/books-list.blade.php
@@ -1,11 +1,16 @@
-@foreach($notificationData['chapters'] as $chapter)
+@foreach($books as $chapter)
     <div>
-        <h3>タイトル：{{ $chapter['chapter_title'] }}</h3>
-        <img src="{{ $chapter['chapter_cover_url'] }}"
-             alt="{{ $chapter['chapter_title'] }}"
-             width="250"
-             style="display: block; margin: 0 auto 10px;">
-        <h3>サブタイトル：<em>{{ $chapter['chapter_second_title'] }}</em></h3>
-        <h3>第<em>{{ $chapter['chapter_number'] }}巻</em></h3>
+        @if(data_get($chapter, 'cover_artwork_url'))
+            <img
+                src="{{ data_get($chapter, 'cover_artwork_url') }}"
+                alt="{{ data_get($chapter, 'title') }}"
+                width="250"
+                style="display: block; margin: 0 auto 10px;"
+            >
+        @endif
+
+        <p><strong>タイトル：</strong>{{ data_get($chapter, 'title') }}</p>
+        <p><strong>サブタイトル：</strong><em>{{ data_get($chapter, 'second_title') }}</em></p>
+        <p>第<em>{{ data_get($chapter, 'chapter_number') }}</em>巻</p>
     </div>
 @endforeach
