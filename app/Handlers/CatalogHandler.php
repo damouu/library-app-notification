2bd27a2cc5e6dc299c3f68d4a7a941a343708d72
damouu <mouadsehbaoui@gmail.com>
2026-06-28 15:14:46 +0900

    Refactor: CatalogHandler.php (#LAN-4)

--- app/Handlers/CatalogHandler.php
+++ app/Handlers/CatalogHandler.php
@@ -3,6 +3,7 @@
 namespace App\Handlers;
 
 use App\DTO\ChapterCreatedEventDTO;
+use App\Repositories\ProcessedEventRepository;
 use App\Services\CatalogProjectionService;
 use Junges\Kafka\Contracts\ConsumerMessage;
 use Junges\Kafka\Contracts\MessageConsumer;
@@ -10,13 +11,17 @@ use Junges\Kafka\Contracts\MessageConsumer;
 class CatalogHandler
 {
 
-    public function __construct(private readonly CatalogProjectionService $catalogProjectionService)
-    {
-    }
+    public function __construct(
+        private readonly CatalogProjectionService $catalogProjectionService,
+        private readonly ProcessedEventRepository $eventRepository,
+    ){}
 
     public function __invoke(ConsumerMessage $message, MessageConsumer $consumer): void
     {
         $event = ChapterCreatedEventDTO::fromArray((array)$message->getBody());
+        if ($this->eventRepository->exists($event->metadataDTO->eventUuid)) {
+            return;
+        }
         $this->catalogProjectionService->handle($event);
     }
 }
