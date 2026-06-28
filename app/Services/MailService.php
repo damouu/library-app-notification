42e9ef5f7b6ff9b298aff08f4f37aefcbbd989c5
damouu <mouadsehbaoui@gmail.com>
2026-06-28 15:20:21 +0900

    Remove: MailService.php (#LAN-4)

---
+++ app/Services/MailService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use App\Mail\UserRegisterMail;
+use App\Models\UserProjection;
+use Illuminate\Support\Facades\Mail;
+
+class MailService
+{
+    public function sendUserRegistered(UserProjection $user): void
+    {
+        Mail::to($user->email)->send(new UserRegisterMail($user));
+    }
+}
