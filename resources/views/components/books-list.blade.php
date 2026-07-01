c0bc2e148438b14ead59044d9bb7fc3c7f8acd75
damouu <mouadsehbaoui@gmail.com>
2026-07-01 20:09:34 +0900

    Design: books-list.blade.php (#LAN-8)

--- resources/views/components/books-list.blade.php
+++ resources/views/components/books-list.blade.php
@@ -1,16 +1,18 @@
 @foreach($books as $chapter)
     <div>
-        @if(data_get($chapter, 'cover_artwork_url'))
-            <img
-                src="{{ data_get($chapter, 'cover_artwork_url') }}"
-                alt="{{ data_get($chapter, 'title') }}"
-                width="250"
-                style="display: block; margin: 0 auto 10px;"
-            >
-        @endif
+        <a href="http://localhost:3002/chapters/{{ data_get($chapter, 'chapter_uuid') }}">
+            @if(data_get($chapter, 'cover_artwork_url'))
+                <img
+                    src="{{ data_get($chapter, 'cover_artwork_url') }}"
+                    alt="{{ data_get($chapter, 'title') }}"
+                    width="250"
+                    style="display: block; margin: 0 auto 10px;"
+                >
+            @endif
 
-        <p><strong>タイトル：</strong>{{ data_get($chapter, 'title') }}</p>
-        <p><strong>サブタイトル：</strong><em>{{ data_get($chapter, 'second_title') }}</em></p>
-        <p>第<em>{{ data_get($chapter, 'chapter_number') }}</em>巻</p>
+            <p><strong>タイトル：</strong>{{ data_get($chapter, 'title') }}</p>
+            <p><strong>サブタイトル：</strong><em>{{ data_get($chapter, 'second_title') }}</em></p>
+            <p>第<em>{{ data_get($chapter, 'chapter_number') }}</em>巻</p>
+        </a>
     </div>
 @endforeach
