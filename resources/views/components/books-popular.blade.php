11a1068757bcd64d240aedafb538c6ba2f5f37a1
damouu <mouadsehbaoui@gmail.com>
2026-07-01 20:09:46 +0900

    Design: books-popular.blade.php (#LAN-8)

---
+++ resources/views/components/books-popular.blade.php
@@ -0,0 +1,10 @@
+<div style="margin-bottom: 16px;">
+    <h3 style="margin: 0 0 12px; font-size: 20px; color: #1f2937;">
+        📚 人気の作品をご紹介
+    </h3>
+
+    <p style="margin: 0; font-size: 15px; color: #4b5563; line-height: 1.6;">
+        現在、多くの利用者に読まれている作品をご紹介します。<br>
+        気になる作品があれば、ぜひチェックしてみてください。
+    </p>
+</div>
