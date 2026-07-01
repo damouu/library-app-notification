dd30617394dee07b76bd63eb69d64ee43160822b
damouu <mouadsehbaoui@gmail.com>
2026-07-01 20:12:14 +0900

    Design: user-registration.blade.php (#LAN-8)

---
+++ resources/views/components/user-registration.blade.php
@@ -0,0 +1,6 @@
+<h1 style="font-size: 24px; color: #111827; text-align: center; margin-bottom: 8px;">
+    👋 ようこそ、図書館アプリへ！
+</h1>
+<p style="font-size: 18px; color: #4b5563; text-align: center; margin-top: 0;">
+    ご登録ありがとうございます。
+</p>
