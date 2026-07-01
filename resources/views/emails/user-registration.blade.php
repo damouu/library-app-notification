4706393f9f0a5ed2003eb411d4c6f52a5deb46ad
damouu <mouadsehbaoui@gmail.com>
2026-07-01 20:12:24 +0900

    Design: user-registration.blade.php (#LAN-8)

--- resources/views/emails/user-registration.blade.php
+++ resources/views/emails/user-registration.blade.php
@@ -1,5 +1,53 @@
-<h1>図書館のアップリご利用頂き誠にありがとうが座います!</h1>
-<p>email <strong>{{ $userProjection->email }}</strong>.</p>
-<p>membercard <strong>{{ $userProjection->member_card_uuid }}</strong>.</p>
-<p>user name <strong>{{ $userProjection->user_name }}</strong>.</p>
-<img src="{{ $userProjection->avatar_img_url }}" alt="User avatar">
+<head>
+    <style>
+        {!! file_get_contents('resources/css/app.css') !!}
+    </style>
+</head>
+
+<div class="user-email">
+
+    <div class="borrow-header">
+        <x-borrow-header/>
+    </div>
+
+    <div style="margin-top: 30px;">
+        <div class="appli-title">
+            <x-user-registration/>
+        </div>
+
+        <div style="margin-top: 24px; border-bottom: 1px solid #eee;">
+            <x-user-welcome-message :user-projection="$userProjection"/>
+            <x-user-membercard :user-projection="$userProjection"/>
+        </div>
+    </div>
+
+    <div style="margin-top: 40px;">
+        <div class="appli-title">
+            <x-books-popular/>
+        </div>
+
+        <div class="chapters-list">
+            <x-books-list :books="$popularChapters"/>
+        </div>
+
+        <div style="margin-top: 24px; border-bottom: 1px solid #eee;">
+            <x-catalog-button url="http://localhost:3002/series?page=1"/>
+            <p style="text-align:center; color:#6b7280; font-size: 14px; margin-top: 16px;">
+                あなたにぴったりの一冊が見つかりますように。📚
+            </p>
+        </div>
+
+        <div style="text-align: center; border-bottom: 1px solid #eee;">
+            <x-use-explanation/>
+        </div>
+
+        <div>
+            <x-use-reminder/>
+        </div>
+    </div>
+
+    <div style="margin-top: 40px;">
+        <x-user-register-footer/>
+    </div>
+
+</div>
