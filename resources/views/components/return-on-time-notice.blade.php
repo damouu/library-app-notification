dbe9333e42fa109a63f34d1b79aaac4c4669cf8b
damouu <mouadsehbaoui@gmail.com>
2026-07-12 11:31:37 +0900

    Refactor: return-on-time-notice.blade.php (#LAN-10)

--- resources/views/components/return-on-time-notice.blade.php
+++ resources/views/components/return-on-time-notice.blade.php
@@ -3,7 +3,7 @@
     <p style="color: #2d5a04; font-weight: bold; margin: 0; font-size: 16px; text-align: center;">
         【完了】図書返却受領のお知らせ
     </p>
-    <p style="color: #333333; font-size: 14px; line-height: 1.6; margin: 10px 0 0 0; text-align: center;">
+    <p class="return-on-time">
         図書の返却手続きが正常に完了いたしました。ご協力ありがとうございました。
     </p>
 </div>
