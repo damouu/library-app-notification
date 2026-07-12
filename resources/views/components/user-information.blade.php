93b29b4a84ba400796d510028e60ba70867e5e07
damouu <mouadsehbaoui@gmail.com>
2026-07-12 10:48:19 +0900

    Refactor: user-information.blade.php (#LAN-9)

--- resources/views/components/user-information.blade.php
+++ resources/views/components/user-information.blade.php
@@ -1,15 +1,17 @@
 @props(['userProjection'])
 
-<table role="presentation" cellpadding="0" cellspacing="0" border="0">
+<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" style="margin: 0 auto;">
     <tr>
-        <td style="vertical-align: middle; padding-right: 10px;">
+        <td style="vertical-align: middle; padding-right: 5px;">
             <img
                 src="{{ $userProjection->avatar_img_url }}"
                 alt="User avatar"
                 class="user-avatar"
+                style="display: block; border-radius: 50%; object-fit: cover;"
             >
         </td>
-        <td style="vertical-align: middle;">
+
+        <td style="vertical-align: middle; font-size: 22px; color: #ffffff;">
             <strong>{{ $userProjection->user_name }}様</strong>
         </td>
     </tr>
