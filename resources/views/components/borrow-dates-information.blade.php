9e9f0eb5ae010684c6fb607adc9273e5ac8ab0cf
damouu <mouadsehbaoui@gmail.com>
2026-07-12 10:47:53 +0900

    Refactor: borrow-dates-information.blade.php (#LAN-9)

--- resources/views/components/borrow-dates-information.blade.php
+++ resources/views/components/borrow-dates-information.blade.php
@@ -1,5 +1,5 @@
 @props(['borrowStartDate', 'borrowEndDate'])
 
 
-<h3>貸出日は： {{ $borrowStartDate }} </h3>
-<h3 style="color: orangered"> 返却期限日は： {{ $borrowEndDate }}   </h3>
+<h3>貸出日： {{ $borrowStartDate }} </h3>
+<h3 style="color: orange"> 返却期限日： {{ $borrowEndDate }}   </h3>
